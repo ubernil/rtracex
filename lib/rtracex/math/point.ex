abc9bda133f94c5288e626ec2f67652e66489373
Anton Sidorov <anton@ubernil.com>
2020-05-05 17:44:02 +0300

    point: add new/3

--- lib/rtracex/math/point.ex
+++ lib/rtracex/math/point.ex
@@ -16,6 +16,12 @@ defmodule Rtracex.Math.Point do
         }
 
   @doc """
+  Creates new point with it's coordinates.
+  """
+  @spec new(float(), float(), float()) :: __MODULE__.t()
+  def new(x, y, z), do: {x, y, z, :point}
+
+  @doc """
   Checks if a given Tstore object is a point.
 
   iex> Rtracex.Math.Point.is_a?({0.0, 0.0, 0.0, :point})
@@ -27,4 +33,13 @@ defmodule Rtracex.Math.Point do
   @spec is_a?(__MODULE__.t() | Tstore.t()) :: boolean()
   def is_a?({_x, _y, _z, :point}), do: true
   def is_a?({_x, _y, _z, _any_other}), do: false
+
+  @spec x(__MODULE__.t()) :: float()
+  defdelegate x(point), to: Rtracex.Math.Tstore
+
+  @spec y(__MODULE__.t()) :: float()
+  defdelegate y(point), to: Rtracex.Math.Tstore
+
+  @spec z(__MODULE__.t()) :: float()
+  defdelegate z(point), to: Rtracex.Math.Tstore
 end
--- test/rtracex/math/point_test.exs
+++ test/rtracex/math/point_test.exs
@@ -1,4 +1,16 @@
 defmodule Rtracex.Math.PointTest do
   use ExUnit.Case
+  alias Rtracex.Math.Point
+
   doctest Rtracex.Math.Point
+
+  describe "creating new Point" do
+    test "with new/3 function" do
+      tested = Point.new(1.0, 2.0, 3.0)
+      assert Point.is_a?(tested)
+      assert Point.x(tested) == 1.0
+      assert Point.y(tested) == 2.0
+      assert Point.z(tested) == 3.0
+    end
+  end
 end
