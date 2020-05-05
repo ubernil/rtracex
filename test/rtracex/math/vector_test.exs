430890513fbe90658def0ed605de863b9f6b48d7
Anton Sidorov <anton@ubernil.com>
2020-05-05 17:44:10 +0300

    vector: add new/3

--- test/rtracex/math/vector_test.exs
+++ test/rtracex/math/vector_test.exs
@@ -1,4 +1,16 @@
 defmodule Rtracex.Math.VectorTest do
   use ExUnit.Case
+  alias Rtracex.Math.Vector
+
   doctest Rtracex.Math.Vector
+
+  describe "creating new Vector" do
+    test "with new/3 function" do
+      tested = Vector.new(1.0, 2.0, 3.0)
+      assert Vector.is_a?(tested)
+      assert Vector.x(tested) == 1.0
+      assert Vector.y(tested) == 2.0
+      assert Vector.z(tested) == 3.0
+    end
+  end
 end
