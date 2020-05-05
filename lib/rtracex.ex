83277f1dde3aae6ea0cc3cce3b94bb128578b233
Anton Sidorov <anton@ubernil.com>
2020-05-05 14:24:37 +0300

    initial commit

---
+++ lib/rtracex.ex
@@ -0,0 +1,18 @@
+defmodule Rtracex do
+  @moduledoc """
+  Documentation for `Rtracex`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Rtracex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/rtracex_test.exs
@@ -0,0 +1,8 @@
+defmodule RtracexTest do
+  use ExUnit.Case
+  doctest Rtracex
+
+  test "greets the world" do
+    assert Rtracex.hello() == :world
+  end
+end
